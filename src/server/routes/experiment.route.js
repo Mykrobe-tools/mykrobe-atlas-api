3322a58124ed0b8ee7bf59bfba3f97eaefc3cca8
Yassire Elhani <yassire@makeandship.com>
2017-04-05 11:49:51 +0100

    update the doc

--- src/server/routes/experiment.route.js
+++ src/server/routes/experiment.route.js
@@ -6,56 +6,147 @@ import config from '../../config/env';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
-router.route('/')
+
   /**
-   * @api {get} /user Get current user details
-   *
-   * @apiName Read user
-   * @apiGroup Current User
-   * @apiUse Header
-   * @apiUse NotFound
-   *
+   * @apiDefine ExperimentResponse
    * @apiSuccessExample Success-Response:
    *     HTTP/1.1 200 OK
    *     {
    *       "status": "success",
    *       "data": {
-   *                "firstname": "John",
-   *                "lastname": "Kitting",
-   *                "phone": "07686833972",
-   *                "email": "john@nhs.co.uk",
-   *                "id": "588624076182796462cb133e"
-   *              }
+   *         "owner": {
+   *           "firstname": "Sean",
+   *           "lastname": "Leavy",
+   *           "phone": "+44 7968 716851",
+   *           "email": "sean@gmail.com",
+   *           "id": "58e2697c87f269174c48cedf"
+   *         },
+   *         "organisation": {
+   *           "name": "Apex Entertainment"
+   *         },
+   *         "location": {
+   *           "name": "London",
+   *           "lat": 3.4,
+   *           "lng": -2.3,
+   *           "id": "58e4c7526555100f447d50ef"
+   *         },
+   *         "collected": "2017-04-17T00:00:00.000Z",
+   *         "uploaded": "2017-04-20T00:00:00.000Z",
+   *         "jaccardIndex": {
+   *           "analysed": "2017-04-20T00:00:00.000Z",
+   *           "engine": "",
+   *           "version": "1.0",
+   *           "experiments": [],
+   *           "id": "58e4c7526555100f447d50ee"
+   *         },
+   *           "snpDistance": {
+   *             "analysed": "2017-04-21T00:00:00.000Z",
+   *             "engine": "",
+   *             "version": "1.0",
+   *             "experiments": [],
+   *             "id": "58e4c7526555100f447d50ed"
+   *         },
+   *           "geoDistance": {
+   *             "analysed": "2017-04-22T00:00:00.000Z",
+   *             "engine": "",
+   *             "version": "1.0",
+   *             "experiments": [],
+   *             "id": "58e4c7526555100f447d50ec"
+   *           },
+   *         "id": "58e4c7526555100f447d50eb"
+   *       }
+   *     }
+   */
+router.route('/')
+  /**
+   * @api {get} /experiments Get experiments list
+   *
+   * @apiName List experiments
+   * @apiGroup Experiments
+   * @apiUse Header
+   *
+   * @apiSuccessExample Success-Response:
+   *     HTTP/1.1 200 OK
+   *     {
+   *       "status": "success",
+   *       "data": [{
+   *         "owner": {
+   *           "firstname": "Sean",
+   *           "lastname": "Leavy",
+   *           "phone": "+44 7968 716851",
+   *           "email": "sean@gmail.com",
+   *           "id": "58e2697c87f269174c48cedf"
+   *         },
+   *         "organisation": {
+   *           "name": "Apex Entertainment"
+   *         },
+   *         "location": {
+   *           "name": "London",
+   *           "lat": 3.4,
+   *           "lng": -2.3,
+   *           "id": "58e4c7526555100f447d50ef"
+   *         },
+   *         "collected": "2017-04-17T00:00:00.000Z",
+   *         "uploaded": "2017-04-20T00:00:00.000Z",
+   *         "jaccardIndex": {
+   *           "analysed": "2017-04-20T00:00:00.000Z",
+   *           "engine": "",
+   *           "version": "1.0",
+   *           "experiments": [],
+   *           "id": "58e4c7526555100f447d50ee"
+   *         },
+   *           "snpDistance": {
+   *             "analysed": "2017-04-21T00:00:00.000Z",
+   *             "engine": "",
+   *             "version": "1.0",
+   *             "experiments": [],
+   *             "id": "58e4c7526555100f447d50ed"
+   *         },
+   *           "geoDistance": {
+   *             "analysed": "2017-04-22T00:00:00.000Z",
+   *             "engine": "",
+   *             "version": "1.0",
+   *             "experiments": [],
+   *             "id": "58e4c7526555100f447d50ec"
+   *           },
+   *         "id": "58e4c7526555100f447d50eb"
+   *       }]
    *     }
    */
   .get(expressJwt({ secret: config.jwtSecret }),
        experimentController.list)
   /**
-   * @api {put} /user Update current user details
+   * @api {post} /experiments Create new experiment
    *
-   * @apiName Update current user
-   * @apiGroup Current User
+   * @apiName Create experiment
+   * @apiGroup Experiments
    * @apiUse Header
-   * @apiUse NotFound
+   * @apiUse ExperimentResponse
    *
-   * @apiParam {String} [firstname] The user firstname.
-   * @apiParam {String} [lastname] The user lastname.
-   * @apiParam {String} [phone] The user phone.
-   * @apiParam {String} [email] The user email.
-   * @apiParam {Array} [buddies] The user buddies.
+   * @apiParam {Object} organisation The experiment organisation.
+   * @apiParam {String} organisation.name The name of organisation.
+   * @apiParam {Object} location The experiment location.
+   * @apiParam {String} location.name The location name.
+   * @apiParam {Number} location.lat The location lat.
+   * @apiParam {Number} location.lng The location lng.
+   * @apiParam {Date} collected The collection date.
+   * @apiParam {Date} uploaded The upload date.
+   * @apiParam {Object} jaccardIndex The experiment jaccardIndex.
+   * @apiParam {Date} jaccardIndex.analysed The analysis date.
+   * @apiParam {String} jaccardIndex.engine The engine.
+   * @apiParam {String} jaccardIndex.version The version.
+   * @apiParam {Array} jaccardIndex.experiments The experiments.
+   * @apiParam {Object} snpDistance The experiment snpDistance.
+   * @apiParam {Date} snpDistance.analysed The analysis date.
+   * @apiParam {String} snpDistance.engine The engine.
+   * @apiParam {String} snpDistance.version The version.
+   * @apiParam {Array} snpDistance.experiments The experiments.
+   * @apiParam {Object} geoDistance The experiment geoDistance.
+   * @apiParam {Date} geoDistance.analysed The analysis date.
+   * @apiParam {String} geoDistance.engine The engine.
+   * @apiParam {String} geoDistance.version The version.
+   * @apiParam {Array} geoDistance.experiments The experiments.
    *
-   * @apiSuccessExample Success-Response:
-   *     HTTP/1.1 200 OK
-   *     {
-   *       "status": "success",
-   *       "data": {
-   *                "firstname": "John",
-   *                "lastname": "Kitting",
-   *                "phone": "07686833972",
-   *                "email": "john@nhs.co.uk",
-   *                "id": "588624076182796462cb133e"
-   *              }
-   *     }
    */
   .post(expressJwt({ secret: config.jwtSecret }),
        userController.loadCurrentUser,
