d0c958ec432ac47019b0510c95c8ccc0e605a0af
Yassire Elhani <yassire@makeandship.com>
2018-08-31 14:38:06 +0100

    WIP distance endpoint

---
+++ src/server/helpers/PredictorResultParser.js
@@ -0,0 +1,158 @@
+const SUSCEPTIBILITY = "susceptibility";
+const PHYLOGENETICS = "phylogenetics";
+const VARIANT_CALLS = "variant_calls";
+const SEQUENCE_CALLS = "sequence_calls";
+const KMER = "kmer";
+const PROBE_SETS = "probe_sets";
+const FILES = "files";
+const VERSION = "version";
+const GENOTYPE_MODEL = "genotype_model";
+const PREDICT = "predict";
+const CALLED_BY = "called_by";
+const RESISTANT = "R";
+const PERCENT_COVERAGE = "percent_coverage";
+const MEDIAN_DEPTH = "median_depth";
+const EXTERNAL_ID = "external_id";
+const ANALYSED = "analysed";
+
+class PredictorResultParser {
+  static parse(predictorNamedResult) {
+    const result = {
+      type: "predictor",
+      received: new Date()
+    };
+    if (predictorNamedResult.result) {
+      const predictorResult = this.getPredictorResult(
+        predictorNamedResult.result
+      );
+      if (predictorResult) {
+        const keys = Object.keys(predictorResult);
+        for (var i = 0; i < keys.length; i++) {
+          // use oldschool loop for performance
+          const attribute = keys[i];
+          switch (attribute) {
+            case SUSCEPTIBILITY:
+              result.susceptibility = this.parseSusceptibility(
+                predictorResult[attribute]
+              );
+              break;
+            case PHYLOGENETICS:
+              result.phylogenetics = this.parsePhylogenetics(
+                predictorResult[attribute]
+              );
+              break;
+            case VARIANT_CALLS:
+              result.variantCalls = predictorResult[attribute];
+              break;
+            case SEQUENCE_CALLS:
+              result.sequenceCalls = predictorResult[attribute];
+              break;
+            case KMER:
+              result.kmer = predictorResult[attribute];
+              break;
+            case PROBE_SETS:
+              result.probeSets = predictorResult[attribute];
+              break;
+            case FILES:
+              result.files = predictorResult[attribute];
+              break;
+            case VERSION:
+              result.version = predictorResult[attribute];
+              break;
+            case GENOTYPE_MODEL:
+              result.genotypeModel = predictorResult[attribute];
+              break;
+            case EXTERNAL_ID:
+              result.externalId = predictorResult[attribute];
+              break;
+            case ANALYSED:
+              result.analysed = new Date();
+              break;
+          }
+        }
+      }
+    }
+
+    return result;
+  }
+
+  /**
+   * Extract the results from a result object
+   * {
+   *   someName: { .. result here .. }
+   * }
+   * @param {object} predictorNamedResult
+   * @return {object} result
+   */
+  static getPredictorResult(predictorNamedResult) {
+    let predictorResult = null;
+    if (predictorNamedResult) {
+      // assumption is there is only one wrapping name in predictor results
+      const keys = Object.keys(predictorNamedResult);
+      if (keys && keys.length) {
+        const first = keys[0];
+        const namedResult = predictorNamedResult[first];
+        namedResult[EXTERNAL_ID] = first;
+        return namedResult;
+      }
+    }
+    return predictorResult;
+  }
+
+  /**
+   * Parse a susceptibility sub schema and reshape it to store in the db
+   * @param {object} predictorSusceptibility
+   * @return {array} susceptibility
+   */
+  static parseSusceptibility(predictorSusceptibility) {
+    const susceptibility = [];
+    if (predictorSusceptibility) {
+      Object.keys(predictorSusceptibility).forEach(drug => {
+        const drugSusceptibility = {
+          name: drug
+        };
+
+        const results = predictorSusceptibility[drug];
+        const prediction = results[PREDICT];
+        if (prediction) {
+          drugSusceptibility.prediction = prediction;
+          if (prediction === RESISTANT) {
+            drugSusceptibility.calledBy = results[CALLED_BY];
+          }
+        }
+
+        susceptibility.push(drugSusceptibility);
+      });
+    }
+    return susceptibility;
+  }
+
+  /**
+   * Parse a phylogenetics sub schema and reshape it to store in the db
+   * @param {object} predictorPhylogenetics
+   * @return {array} phylogenetics
+   */
+  static parsePhylogenetics(predictorPhylogenetics) {
+    const phylogenetics = [];
+
+    if (predictorPhylogenetics) {
+      Object.keys(predictorPhylogenetics).forEach(type => {
+        const results = predictorPhylogenetics[type];
+        Object.keys(results).forEach(result => {
+          const typePhylogenetics = {
+            type: type
+          };
+          typePhylogenetics.result = result;
+          typePhylogenetics.percentCoverage = results[result][PERCENT_COVERAGE];
+          typePhylogenetics.medianDepth = results[result][MEDIAN_DEPTH];
+
+          phylogenetics.push(typePhylogenetics);
+        });
+      });
+    }
+
+    return phylogenetics;
+  }
+}
+
+export default PredictorResultParser;
