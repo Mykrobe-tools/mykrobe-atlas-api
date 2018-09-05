2748150f1af8c039159e53e65ef0c3512abec61b
Yassire Elhani <yassire@makeandship.com>
2018-09-05 08:59:17 +0100

    add inflate results tests

--- src/server/tests/helpers/NearestNeighboursResultParser.test.js
+++ src/server/tests/helpers/NearestNeighboursResultParser.test.js
@@ -10,11 +10,11 @@ describe("NearestNeighboursResultParser", () => {
       expect(result).toHaveProperty("type");
       expect(result).toHaveProperty("subType");
       expect(result).toHaveProperty("received");
-      expect(result).toHaveProperty("nearestNeighbours");
+      expect(result).toHaveProperty("experiments");
 
-      expect(result.type).toEqual("distance");
+      expect(result.type).toEqual("nearestNeighbours");
       expect(result.subType).toEqual("Nearest neighbours");
-      expect(result.nearestNeighbours.length).toEqual(9);
+      expect(result.experiments.length).toEqual(9);
 
       done();
     });
@@ -22,11 +22,11 @@ describe("NearestNeighboursResultParser", () => {
     it("should create nearest neighbours array", done => {
       const parser = new NearestNeighboursResultParser(NEAREST_NEIGHBOURS);
       const result = parser.parse();
-      const nearestNeighbours = result.nearestNeighbours;
+      const nearestNeighbours = result.experiments;
 
       expect(nearestNeighbours.length).toEqual(9);
       nearestNeighbours.forEach(nearestNeighbour => {
-        expect(nearestNeighbour).toHaveProperty("experimentId");
+        expect(nearestNeighbour).toHaveProperty("id");
         expect(nearestNeighbour).toHaveProperty("distance");
       });
 
