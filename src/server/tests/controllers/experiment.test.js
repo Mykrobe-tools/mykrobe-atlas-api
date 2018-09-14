e231b2786395893fdccc9f0b5c10e8c52b3953cf
Mark Thomsit <mark@ceremium.com>
2018-09-14 09:20:16 +0100

    Run all experiment tests

--- src/server/tests/controllers/experiment.test.js
+++ src/server/tests/controllers/experiment.test.js
@@ -574,9 +574,9 @@ describe("## Experiment APIs", () => {
         .attach("file", "src/server/tests/fixtures/files/333-08.json")
         .expect(httpStatus.OK)
         .end((err, res) => {
-          const filePath = `${config.express.uploadDir}/experiments/${
-            id
-          }/file/333-08.json`;
+          const filePath = `${
+            config.express.uploadDir
+          }/experiments/${id}/file/333-08.json`;
           expect(res.body.status).toEqual("success");
           expect(res.body.data).toEqual("File uploaded and reassembled");
           expect(fs.existsSync(filePath)).toEqual(true);
@@ -602,9 +602,9 @@ describe("## Experiment APIs", () => {
         .attach("file", "src/server/tests/fixtures/files/333-08.json")
         .expect(httpStatus.OK)
         .end((err, res) => {
-          const filePath = `${config.express.uploadDir}/experiments/${
-            id
-          }/file/333-08.json`;
+          const filePath = `${
+            config.express.uploadDir
+          }/experiments/${id}/file/333-08.json`;
           expect(res.body.status).toEqual("success");
           expect(res.body.data).toEqual("File uploaded and reassembled");
           expect(fs.existsSync(filePath)).toEqual(true);
@@ -898,9 +898,9 @@ describe("## Experiment APIs", () => {
                 job = await findJob(jobs, id, "call analysis api");
               }
               expect(job.data.file).toEqual(
-                `${config.express.uploadsLocation}/experiments/${
-                  id
-                }/file/333-08.json`
+                `${
+                  config.express.uploadsLocation
+                }/experiments/${id}/file/333-08.json`
               );
               expect(job.data.sample_id).toEqual(id);
               expect(job.data.attempt).toEqual(0);
@@ -949,9 +949,9 @@ describe("## Experiment APIs", () => {
           })
           .expect(httpStatus.OK)
           .end((err, res) => {
-            const filePath = `${config.express.uploadDir}/experiments/${
-              id
-            }/file/fake.json`;
+            const filePath = `${
+              config.express.uploadDir
+            }/experiments/${id}/file/fake.json`;
             expect(res.body.status).toEqual("success");
             expect(res.body.data).toEqual("Download started from dropbox");
             expect(fs.existsSync(filePath)).toEqual(true);
@@ -994,9 +994,9 @@ describe("## Experiment APIs", () => {
                 job = await findJob(jobs, id, "call analysis api");
               }
               expect(job.data.file).toEqual(
-                `${config.express.uploadsLocation}/experiments/${
-                  id
-                }/file/333-08.json`
+                `${
+                  config.express.uploadsLocation
+                }/experiments/${id}/file/333-08.json`
               );
               expect(job.data.sample_id).toEqual(id);
               expect(job.data.attempt).toEqual(0);
@@ -1045,9 +1045,9 @@ describe("## Experiment APIs", () => {
           })
           .expect(httpStatus.OK)
           .end((err, res) => {
-            const filePath = `${config.express.uploadDir}/experiments/${
-              id
-            }/file/fake.json`;
+            const filePath = `${
+              config.express.uploadDir
+            }/experiments/${id}/file/fake.json`;
             expect(res.body.status).toEqual("success");
             expect(res.body.data).toEqual("Download started from box");
             expect(fs.existsSync(filePath)).toEqual(true);
@@ -1092,9 +1092,9 @@ describe("## Experiment APIs", () => {
                 job = await findJob(jobs, id, "call analysis api");
               }
               expect(job.data.file).toEqual(
-                `${config.express.uploadsLocation}/experiments/${
-                  id
-                }/file/333-08.json`
+                `${
+                  config.express.uploadsLocation
+                }/experiments/${id}/file/333-08.json`
               );
               expect(job.data.sample_id).toEqual(id);
               expect(job.data.attempt).toEqual(0);
@@ -1164,9 +1164,9 @@ describe("## Experiment APIs", () => {
           })
           .expect(httpStatus.OK)
           .end((err, res) => {
-            const filePath = `${config.express.uploadDir}/experiments/${
-              id
-            }/file/fake.json`;
+            const filePath = `${
+              config.express.uploadDir
+            }/experiments/${id}/file/fake.json`;
             expect(res.body.status).toEqual("success");
             expect(res.body.data).toEqual("Download started from googleDrive");
             expect(fs.existsSync(filePath)).toEqual(true);
@@ -1209,9 +1209,9 @@ describe("## Experiment APIs", () => {
                 job = await findJob(jobs, id, "call analysis api");
               }
               expect(job.data.file).toEqual(
-                `${config.express.uploadsLocation}/experiments/${
-                  id
-                }/file/333-08.json`
+                `${
+                  config.express.uploadsLocation
+                }/experiments/${id}/file/333-08.json`
               );
               expect(job.data.sample_id).toEqual(id);
               expect(job.data.attempt).toEqual(0);
@@ -1260,9 +1260,9 @@ describe("## Experiment APIs", () => {
           })
           .expect(httpStatus.OK)
           .end((err, res) => {
-            const filePath = `${config.express.uploadDir}/experiments/${
-              id
-            }/file/fake.json`;
+            const filePath = `${
+              config.express.uploadDir
+            }/experiments/${id}/file/fake.json`;
             expect(res.body.status).toEqual("success");
             expect(res.body.data).toEqual("Download started from oneDrive");
             expect(fs.existsSync(filePath)).toEqual(true);
@@ -1297,9 +1297,9 @@ describe("## Experiment APIs", () => {
                 job = await findJob(jobs, id, "call analysis api");
               }
               expect(job.data.file).toEqual(
-                `${config.express.uploadsLocation}/experiments/${
-                  id
-                }/file/333-08.json`
+                `${
+                  config.express.uploadsLocation
+                }/experiments/${id}/file/333-08.json`
               );
               expect(job.data.sample_id).toEqual(id);
               expect(job.data.attempt).toEqual(0);
@@ -1336,9 +1336,9 @@ describe("## Experiment APIs", () => {
             }
             expect(job.name).toEqual("call analysis api");
             expect(job.data.file).toEqual(
-              `${config.express.uploadsLocation}/experiments/${
-                id
-              }/file/333-08.json`
+              `${
+                config.express.uploadsLocation
+              }/experiments/${id}/file/333-08.json`
             );
             expect(job.data.sample_id).toEqual(id);
             done();
@@ -1376,9 +1376,9 @@ describe("## Experiment APIs", () => {
             expect(res.body.data).toEqual("File uploaded and reassembled");
             expect(audit.experimentId).toEqual(id);
             expect(audit.fileLocation).toEqual(
-              `${config.express.uploadsLocation}/experiments/${
-                id
-              }/file/333-08.json`
+              `${
+                config.express.uploadsLocation
+              }/experiments/${id}/file/333-08.json`
             );
             expect(audit.status).toEqual("Successful");
             expect(audit.attempt).toEqual(1);
@@ -1389,7 +1389,7 @@ describe("## Experiment APIs", () => {
             done();
           });
       });
-      it.only("should emit the events to all subscribers", done => {
+      it("should emit the events to all subscribers", done => {
         const mockAnalysisCallback = jest.fn();
         const mockDistanceCallback = jest.fn();
         experimentEventEmitter.on("analysis-started", mockAnalysisCallback);
@@ -1488,9 +1488,9 @@ describe("## Experiment APIs", () => {
 
             expect(foundJobs.length).toEqual(2);
             expect(foundJobs[0].data.file).toEqual(
-              `${config.express.uploadsLocation}/experiments/${
-                id
-              }/file/333-09.json`
+              `${
+                config.express.uploadsLocation
+              }/experiments/${id}/file/333-09.json`
             );
             expect(foundJobs[0].data.sample_id).toEqual(id);
             done();
@@ -1531,9 +1531,9 @@ describe("## Experiment APIs", () => {
             expect(res.body.data).toEqual("File uploaded and reassembled");
             expect(audit.experimentId).toEqual(id);
             expect(audit.fileLocation).toEqual(
-              `${config.express.uploadsLocation}/experiments/${
-                id
-              }/file/333-09.json`
+              `${
+                config.express.uploadsLocation
+              }/experiments/${id}/file/333-09.json`
             );
             expect(audit.status).toEqual("Failed");
             expect(audit.attempt).toEqual(1);
@@ -1573,9 +1573,9 @@ describe("## Experiment APIs", () => {
             expect(res.body.data).toEqual("File uploaded and reassembled");
             expect(audit.experimentId).toEqual(id);
             expect(audit.fileLocation).toEqual(
-              `${config.express.uploadsLocation}/experiments/${
-                id
-              }/file/333-08.json`
+              `${
+                config.express.uploadsLocation
+              }/experiments/${id}/file/333-08.json`
             );
             expect(audit.status).toEqual("Successful");
             expect(audit.attempt).toEqual(1);
@@ -1754,9 +1754,7 @@ describe("## Experiment APIs", () => {
     it("should return the resumable upload status", done => {
       request(app)
         .get(
-          `/experiments/${
-            id
-          }/upload-status?resumableChunkNumber=1&resumableChunkSize=1048576&resumableTotalSize=251726&resumableIdentifier=251726-333-08json&resumableFilename=333-08.json&checksum=4f36e4cbfc9dfc37559e13bd3a309d50`
+          `/experiments/${id}/upload-status?resumableChunkNumber=1&resumableChunkSize=1048576&resumableTotalSize=251726&resumableIdentifier=251726-333-08json&resumableFilename=333-08.json&checksum=4f36e4cbfc9dfc37559e13bd3a309d50`
         )
         .set("Authorization", `Bearer ${token}`)
         .expect(httpStatus.NO_CONTENT)
@@ -1784,9 +1782,7 @@ describe("## Experiment APIs", () => {
         .end(() => {
           request(app)
             .get(
-              `/experiments/${
-                id
-              }/upload-status?resumableChunkNumber=1&resumableChunkSize=1048576&resumableTotalSize=251726&resumableIdentifier=251726-333-08json&resumableFilename=333-08.json&checksum=4f36e4cbfc9dfc37559e13bd3a309d50`
+              `/experiments/${id}/upload-status?resumableChunkNumber=1&resumableChunkSize=1048576&resumableTotalSize=251726&resumableIdentifier=251726-333-08json&resumableFilename=333-08.json&checksum=4f36e4cbfc9dfc37559e13bd3a309d50`
             )
             .set("Authorization", `Bearer ${token}`)
             .expect(httpStatus.NO_CONTENT)
@@ -1799,9 +1795,7 @@ describe("## Experiment APIs", () => {
     it("should be a protected route", done => {
       request(app)
         .get(
-          `/experiments/${
-            id
-          }/upload-status?resumableChunkNumber=1&resumableChunkSize=1048576&resumableTotalSize=251726&resumableIdentifier=251726-333-08json&resumableFilename=333-08.json&checksum=4f36e4cbfc9dfc37559e13bd3a309d50`
+          `/experiments/${id}/upload-status?resumableChunkNumber=1&resumableChunkSize=1048576&resumableTotalSize=251726&resumableIdentifier=251726-333-08json&resumableFilename=333-08.json&checksum=4f36e4cbfc9dfc37559e13bd3a309d50`
         )
         .set("Authorization", "Bearer INVALID_TOKEN")
         .expect(httpStatus.UNAUTHORIZED)
