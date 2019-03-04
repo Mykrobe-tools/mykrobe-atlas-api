27d140d5b4751492e509c302418702cbc814c918
mthomsit <mark@makeandship.co.uk>
2019-03-04 15:26:09 +0000

    Refactor and widen organisation model tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testRegex: "(/test/models/organisation.model.test).[jt]sx?$",
+  testRegex: "(/test/models/.*?test).[jt]sx?$",
   collectCoverage: false,
   testEnvironment: "node",
   reporters: [
