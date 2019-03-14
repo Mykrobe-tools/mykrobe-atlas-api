bc1944994bca97fc163c8794faf54d3a06a8e22a
Yassire Elhani <yassire@makeandship.com>
2019-03-14 16:00:01 +0000

    add unit tests

---
+++ deploy/test.unit.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+set -e
+
+export CURRENT_VERSION=`cat /var/go/releases/server/CURRENT_VERSION`
+
+cd /var/go/releases/server/$CURRENT_VERSION
+
+docker build -t atlas-api-test-unit -f deploy/Dockerfile.test.unit .
+docker run --rm -v /home/ubuntu/binaries/mongo:/app/tmp/mongo atlas-api-test-unit
