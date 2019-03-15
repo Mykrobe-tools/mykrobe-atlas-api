ee045f6ee13c866b7243fe3836f32c74c9c7f852
Yassire Elhani <yassire@makeandship.com>
2019-03-15 11:43:45 +0000

    replace env variables in docker-compose

--- deploy/build.sh
+++ deploy/build.sh
@@ -3,12 +3,11 @@ set -e
 
 export CURRENT_VERSION=`cat /var/go/releases/server/CURRENT_VERSION`
 
-cd /var/go/releases/server/$CURRENT_VERSION
+cd /var/go/releases/server/$CURRENT_VERSION/deploy
 
 
 # build the target container
 # build the image
-cd /var/go/releases/$CURRENT_VERSION/server/deploy
 envsubst < docker-compose.yml > docker-compose.yml.replace
 mv docker-compose.yml.replace docker-compose.yml
 
