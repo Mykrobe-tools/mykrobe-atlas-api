4830015c592ca3311799a5389fd3e817aa4459a2
Yassire Elhani <yassire@makeandship.com>
2019-03-15 14:37:54 +0000

    build the image and push to docker hub

--- deploy/test.unit.sh
+++ deploy/test.unit.sh
@@ -5,4 +5,4 @@ export CURRENT_VERSION=`cat /var/go/releases/server/CURRENT_VERSION`
 cd /var/go/releases/server/$CURRENT_VERSION
 
 docker build -t atlas-api-test-unit -f deploy/Dockerfile.test.unit .
-docker run --rm -v /home/ubuntu/binaries/mongo:/app/tmp/mongo -v /home/ubuntu/reports:/app/reports atlas-api-test-unit
+docker run --rm -v /home/ubuntu/binaries/mongo:/app/tmp/mongo -v /var/go/reports:/app/reports atlas-api-test-unit
