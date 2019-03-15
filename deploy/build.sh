cf8b1d32421f3707be1abc2c68f8722e7906f064
Yassire Elhani <yassire@makeandship.com>
2019-03-15 09:07:36 +0000

    deploy from a dockerhub image

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,23 +1,11 @@
 #/bin/bash
 set -e
 
-export CURRENT_VERSION=`date +"%Y%m%d%H%M"`
+export CURRENT_VERSION=`cat /var/go/releases/server/CURRENT_VERSION`
 
-echo $CURRENT_VERSION > /var/go/releases/server/CURRENT_VERSION
-
-mkdir -p /var/go/releases/server/$CURRENT_VERSION
 cd /var/go/releases/server/$CURRENT_VERSION
 
-cp -rf /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas-api/. .
-
-# copy the ssh keys
-cp /var/go/.ssh/bitbucket-readonly .
-cp /var/go/.ssh/atlas-jsonschema-readonly .
 
 # build the target container
 subber deploy/docker-compose.yml
-docker-compose -f deploy/docker-compose.yml build
 
-# build is done remove the ssh keys
-rm bitbucket-readonly
-rm atlas-jsonschema-readonly
--- deploy/test.unit.sh
+++ deploy/test.unit.sh
@@ -1,9 +1,14 @@
 #/bin/bash
 set -e
 
-export CURRENT_VERSION=`cat /var/go/releases/server/CURRENT_VERSION`
+export CURRENT_VERSION=`date +"%Y%m%d%H%M"`
 
+echo $CURRENT_VERSION > /var/go/releases/server/CURRENT_VERSION
+
+mkdir -p /var/go/releases/server/$CURRENT_VERSION
 cd /var/go/releases/server/$CURRENT_VERSION
+
+cp -rf /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas-api/. .
 
 docker build -t atlas-api-test-unit -f deploy/Dockerfile.test.unit .
 docker run --rm -v /home/ubuntu/binaries/mongo:/app/tmp/mongo atlas-api-test-unit
