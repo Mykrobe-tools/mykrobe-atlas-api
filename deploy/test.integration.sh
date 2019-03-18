08df56c1a4a44b7faf54b63f829555ab6f375a16
Yassire Elhani <yassire@makeandship.com>
2019-03-18 09:41:54 +0000

    Change revert approach

--- deploy/test.integration.sh
+++ deploy/test.integration.sh
@@ -3,7 +3,7 @@ set -e
 
 cd /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas-api
 
-mkdir reports
+mkdir -p reports
 
 docker build -t atlas-api-test-integration -f deploy/Dockerfile.test.integration .
 docker run --rm -v /home/ubuntu/binaries/mongo:/app/tmp/mongo -v /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas-api/reports:/app/reports atlas-api-test-integration
--- deploy/test.unit.sh
+++ deploy/test.unit.sh
@@ -3,7 +3,7 @@ set -e
 
 cd /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas-api
 
-mkdir reports
+mkdir -p reports
 
 docker build -t atlas-api-test-unit -f deploy/Dockerfile.test.unit .
 docker run --rm -v /home/ubuntu/binaries/mongo:/app/tmp/mongo -v /var/lib/go-agent/pipelines/${TARGET_ENVIRONMENT}-atlas-api/reports:/app/reports atlas-api-test-unit
