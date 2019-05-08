1c5ab93d5489733aa3cb8efd683d577cd430b3f3
mthomsit <mark@makeandship.co.uk>
2019-05-08 13:39:05 +0100

    Merge conflicts

--- src/config/services/index.js
+++ src/config/services/index.js
@@ -2,9 +2,6 @@ export default {
   analysisApiUrl: process.env.ANALYSIS_API || "https://cli.mykrobe.com",
   analysisApiMaxRetries: 5,
   analysisApiBackOffPeriod: "in 20 minutes",
-  bigsiApiUrl: process.env.BIGSI_API || "https://cli.mykrobe.com",
-  bigsiApiMaxRetries: 5,
-  bigsiApiBackOffPeriod: "in 20 minutes",
   treeResultsTTL: 4380, // 6 months
   bigsiResultsTTL: 168 // 7 days
 };
