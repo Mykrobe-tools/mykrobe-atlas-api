9763d7a200e15b0c9e62bca1c88907cc622e7743
mthomsit <mark@makeandship.co.uk>
2019-05-23 21:26:14 +0100

    Fix for 'r' resistance

--- test/fixtures/files/results_payloads.js
+++ test/fixtures/files/results_payloads.js
@@ -215,6 +215,305 @@ export default {
       }
     }
   },
+  TRELLO_789: {
+    type: "predictor",
+    result: {
+      SAMEA1015968: {
+        susceptibility: {
+          Ofloxacin: {
+            predict: "S"
+          },
+          Moxifloxacin: {
+            predict: "S"
+          },
+          Isoniazid: {
+            predict: "R",
+            called_by: {
+              "katG_S315X-GCT2155167GGT": {
+                variant: null,
+                genotype: [1, 1],
+                genotype_likelihoods: [-6997.246088906566, -99999999, -71.64953752644888],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 0.0,
+                      median_depth: 0,
+                      min_non_zero_depth: 0,
+                      kmer_count: 0,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 64,
+                      min_non_zero_depth: 57,
+                      kmer_count: 1234,
+                      klen: 20
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 6926
+                },
+                _cls: "Call.VariantCall"
+              }
+            }
+          },
+          Kanamycin: {
+            predict: "R",
+            called_by: {
+              "eis_G-10A-C2715342T": {
+                variant: null,
+                genotype: [1, 1],
+                genotype_likelihoods: [-4533.905643932095, -99999999, -57.17551831959444],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 5.0,
+                      median_depth: 0,
+                      min_non_zero_depth: 12,
+                      kmer_count: 12,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 34,
+                      min_non_zero_depth: 33,
+                      kmer_count: 684,
+                      klen: 21
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 4477
+                },
+                _cls: "Call.VariantCall"
+              }
+            }
+          },
+          Ethambutol: {
+            predict: "r",
+            called_by: {
+              "embA_C-12T-C4243221T": {
+                variant: null,
+                genotype: [0, 1],
+                genotype_likelihoods: [
+                  -2475.5125279500808,
+                  -187.41136722471094,
+                  -305.8407026902221
+                ],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 100.0,
+                      median_depth: 8,
+                      min_non_zero_depth: 7,
+                      kmer_count: 167,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 33,
+                      min_non_zero_depth: 30,
+                      kmer_count: 697,
+                      klen: 21
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 2288
+                },
+                _cls: "Call.VariantCall"
+              },
+              "embB_G406A-GGC4247729GCC": {
+                variant: null,
+                genotype: [0, 1],
+                genotype_likelihoods: [
+                  -1712.3446587511662,
+                  -234.11642316240327,
+                  -296.08925388329317
+                ],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 100.0,
+                      median_depth: 5,
+                      min_non_zero_depth: 4,
+                      kmer_count: 98,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 23,
+                      min_non_zero_depth: 22,
+                      kmer_count: 440,
+                      klen: 20
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 1478
+                },
+                _cls: "Call.VariantCall"
+              }
+            }
+          },
+          Streptomycin: {
+            predict: "R",
+            called_by: {
+              "rpsL_K43R-AAG781686AGG": {
+                variant: null,
+                genotype: [1, 1],
+                genotype_likelihoods: [-4883.818483337196, -99999999, -58.45871590518201],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 0.0,
+                      median_depth: 0,
+                      min_non_zero_depth: 0,
+                      kmer_count: 0,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 38,
+                      min_non_zero_depth: 36,
+                      kmer_count: 727,
+                      klen: 20
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 4825
+                },
+                _cls: "Call.VariantCall"
+              }
+            }
+          },
+          Ciprofloxacin: {
+            predict: "S"
+          },
+          Pyrazinamide: {
+            predict: "r",
+            called_by: {
+              "pncA_P54Q-CGG2289080CTG": {
+                variant: null,
+                genotype: [0, 1],
+                genotype_likelihoods: [-2602.212772716572, -223.5642477336178, -270.4695389482436],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 100.0,
+                      median_depth: 8,
+                      min_non_zero_depth: 7,
+                      kmer_count: 151,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 37,
+                      min_non_zero_depth: 36,
+                      kmer_count: 714,
+                      klen: 20
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 2379
+                },
+                _cls: "Call.VariantCall"
+              }
+            }
+          },
+          Rifampicin: {
+            predict: "R",
+            called_by: {
+              "rpoB_S450X-TCG761154TTG": {
+                variant: null,
+                genotype: [1, 1],
+                genotype_likelihoods: [-4918.802002162891, -99999999, -56.15992495664833],
+                info: {
+                  coverage: {
+                    reference: {
+                      percent_coverage: 0.0,
+                      median_depth: 0,
+                      min_non_zero_depth: 0,
+                      kmer_count: 0,
+                      klen: 21
+                    },
+                    alternate: {
+                      percent_coverage: 100.0,
+                      median_depth: 40,
+                      min_non_zero_depth: 31,
+                      kmer_count: 736,
+                      klen: 20
+                    }
+                  },
+                  expected_depths: [45.0],
+                  contamination_depths: [],
+                  filter: [],
+                  conf: 4863
+                },
+                _cls: "Call.VariantCall"
+              }
+            }
+          },
+          Amikacin: {
+            predict: "S"
+          },
+          Capreomycin: {
+            predict: "S"
+          }
+        },
+        phylogenetics: {
+          phylo_group: {
+            Mycobacterium_tuberculosis_complex: {
+              percent_coverage: 99.675,
+              median_depth: 45.0
+            }
+          },
+          sub_complex: {
+            Unknown: {
+              percent_coverage: -1,
+              median_depth: -1
+            }
+          },
+          species: {
+            Mycobacterium_tuberculosis: {
+              percent_coverage: 98.471,
+              median_depth: 43
+            }
+          },
+          lineage: {
+            Beijing_East_Asia: {
+              percent_coverage: 100.0,
+              median_depth: 39
+            }
+          }
+        },
+        kmer: 21,
+        probe_sets: [
+          "/nfs/leia/research/iqbal/software/mykrobe/mykrobe-atlas-cli/src/mykrobe/data/panels/tb-species-170421.fasta.gz",
+          "/nfs/leia/research/iqbal/software/mykrobe/mykrobe-atlas-cli/src/mykrobe/data/panels/tb-hunt-probe-set-jan-03-2019.fasta.gz"
+        ],
+        files: [
+          "/nfs/leia/research/iqbal/mhunt/Clockwork_ena_tb/Pipeline_root/00/00/02/54/254/Reads/reads.remove_contam.1.1.fq.gz",
+          "/nfs/leia/research/iqbal/mhunt/Clockwork_ena_tb/Pipeline_root/00/00/02/54/254/Reads/reads.remove_contam.1.2.fq.gz"
+        ],
+        version: {
+          "mykrobe-predictor": "v0.7.0",
+          "mykrobe-atlas": "v0.7.0"
+        },
+        genotype_model: "kmer_count"
+      }
+    }
+  },
+
   SUSCEPTIBLE_ALL: {
     type: "predictor",
     result: {
