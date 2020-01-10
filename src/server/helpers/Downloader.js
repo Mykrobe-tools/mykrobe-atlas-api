48f9f8586bd0b95ab6d37012a79aeabe5de88fdf
Yassire Elhani <yassire@makeandship.com>
2020-01-10 09:23:02 +0000

    debug messages

--- src/server/helpers/Downloader.js
+++ src/server/helpers/Downloader.js
@@ -16,15 +16,19 @@ class Downloader {
   }
 
   download(done) {
+    winston.info(`Start downloading ${this.destination} with data ${JSON.stringify(this.data)}`);
     const file = fs.createWriteStream(this.destination);
+    winston.info(`Stream created`);
     https.get(this.options).on("response", res => {
       let downloaded = 0;
       const totalSize = res.headers["content-length"];
       res
         .on("data", async chunk => {
           file.write(chunk);
+          winston.info(`Data chunk received`);
           downloaded += chunk.length;
 
+          winston.info(`Downloaded: ${downloaded}`);
           const experiment = this.data.experiment;
           const status = {
             provider: this.data.provider,
@@ -33,18 +37,23 @@ class Downloader {
             fileLocation: this.options.path
           };
 
+          winston.info(`Sending 3rd-party-upload-progress event`);
           //await EventHelper.updateUploadsState(this.data.user.id, experiment.id, status);
           experimentEventEmitter.emit("3rd-party-upload-progress", {
             experiment,
             status
           });
+
+          winston.info(`3rd-party-upload-progress event emitted.`);
         })
         .on("end", async () => {
+          winston.info(`Download ended`);
+          file.end();
+          winston.info(`File closed`);
           if (done) {
+            winston.info(`Calling the callback function`);
             done();
           }
-          file.end();
-
           const experiment = this.data.experiment;
           const status = {
             provider: this.data.provider,
@@ -53,13 +62,16 @@ class Downloader {
             fileLocation: this.options.path
           };
 
+          winston.info(`Sending 3rd-party-upload-complete event`);
           //await EventHelper.clearUploadsState(this.data.user.id, experiment.id);
           experimentEventEmitter.emit("3rd-party-upload-complete", {
             experiment,
             status
           });
+          winston.info(`3rd-party-upload-complete event emitted.`);
         })
         .on("error", err => {
+          winston.info(`Error during the download.`);
           winston.info(err.message);
         });
     });
