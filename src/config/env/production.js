8cf95d649d121ad2007145d4f5ffe2531ce5fb14
Mark Thomsit <mark@makeandship.com>
2020-06-12 16:43:33 +0100

    Bugfix/1092 upload config (#146)
    * WIP
    
    * Fixes
    
    * Override UPLOADS_DIR in prod
    
    * uploadsDir to uploadDir
    
    * debug resumable
    
    * Debug upload
    
    * Debug upload
    
    * Localise file for Analysis
    
    * Localise file for Analysis
    
    * Increment build
    
    Co-authored-by: mthomsit <mark@makeandship.co.uk>
    Co-authored-by: Yassire Elhani <yassire@makeandship.com>

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -3,6 +3,8 @@ export default {
     uri: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_SERVICE_HOST}:${process.env.DB_SERVICE_PORT}/atlas?replicaSet=${process.env.DB_RS_NAME}`
   },
   express: {
+    uploadDir: process.env.UPLOAD_DIR,
+    analysisApiDir: process.env.ANALYSIS_API_DIR,
     uploadsLocation: process.env.UPLOADS_LOCATION,
     uploadsTempLocation: process.env.UPLOADS_TEMP_LOCATION,
     demoDataRootFolder: process.env.DEMO_DATA_ROOT_FOLDER,
