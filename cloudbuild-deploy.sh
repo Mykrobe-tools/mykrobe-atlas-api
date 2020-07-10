819cf2fc7ec5f186c8b84e1f9de192227f95184e
mthomsit <mark@makeandship.co.uk>
2020-07-10 17:40:15 +0100

    Cloudbuild shell script for update k8s

--- cloudbuild-deploy.sh
+++ cloudbuild-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 TOKEN=$(cat ehk-token.txt)
-curl -XPATCH 'https://45.86.170.176:6443/apis/apps/v1beta2/namespaces/mykrobe-dev/deployments/atlas-api-deployment' -H "Authorization: Bearer $TOKEN" -H 'Content-type: application/json-patch+json' -d "[{ \"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"$IMAGE\" }]" --insecure
+curl -XPATCH 'https://45.86.170.176:6443/apis/apps/v1beta2/namespaces/mykrobe-dev/deployments/atlas-api-deployment' -H "Authorization: Bearer $TOKEN" -H 'Content-type: application/json-patch+json' -d "[{ \"op\": \"replace\", \"path\": \"/spec/template/spec/containers/0/image\", \"value\": \"$1\" }]" --insecure
