db76104be32d1cd097d4dab3b29fc870d0686dcd
Yassire Elhani <yassire@makeandship.com>
2020-10-21 13:57:42 +0100

    resolve files promise

--- src/server/modules/analysis/AnalysisService.js
+++ src/server/modules/analysis/AnalysisService.js
@@ -19,7 +19,7 @@ class AnalysisService {
 
     const uri = `${config.services.analysisApiUrl}/analyses`;
     const payload = {
-      files: ExperimentHelper.localiseFilesForAnalysisApi(experiment.files),
+      files: await ExperimentHelper.localiseFilesForAnalysisApi(experiment.files),
       file: ExperimentHelper.localiseFilepathForAnalysisApi(file),
       sample_id: experiment.sampleId,
       callback_url: `/experiments/${experiment.id}/results`
