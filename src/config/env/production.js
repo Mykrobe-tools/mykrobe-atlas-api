c3aba1c6a6ac7c752e80f9ed3c8359a9ddc37e75
Yassire Elhani <yassire@makeandship.com>
2020-10-30 11:01:52 +0000

    read db credentials from vault

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,6 +1,28 @@
+import fs from "fs";
+
+let username, password;
+const path = "/vault/secrets/db-creds";
+
+if (fs.existsSync(path)) {
+  const data = fs.readFileSync(path, "UTF-8");
+  // split the contents by new line
+  const lines = data.split(/\r?\n/);
+
+  lines.forEach(line => {
+    if (line.startsWith("username:")) {
+      username = line.split(":")[1].trim();
+    } else if (line.startsWith("password:")) {
+      password = line.split(":")[1].trim();
+    }
+  });
+} else {
+  username = process.env.MONGO_USER;
+  password = process.env.MONGO_PASSWORD;
+}
+
 export default {
   db: {
-    uri: `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.DB_SERVICE_HOST}:${process.env.DB_SERVICE_PORT}/atlas?replicaSet=${process.env.DB_RS_NAME}`
+    uri: `mongodb://${username}:${password}@${process.env.DB_SERVICE_HOST}:${process.env.DB_SERVICE_PORT}/atlas?replicaSet=${process.env.DB_RS_NAME}`
   },
   elasticsearch: {
     log: "debug"
