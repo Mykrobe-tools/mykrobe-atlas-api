59f4f8135a70c93e718ba9093a55088b3835f68e
mthomsit <mark@makeandship.co.uk>
2021-02-17 16:18:30 +0000

    Logging in WatchCache

--- src/server/modules/cache/WatchCache.js
+++ src/server/modules/cache/WatchCache.js
@@ -1,4 +1,5 @@
 import Constants from "../../Constants";
+import logger from "../logging/logger";
 import Cache from "./Cache";
 
 const PREFIX = "watch";
@@ -9,24 +10,32 @@ class WatchCache {
   }
 
   async setUser(experimentId, user, expiry = Constants.WATCH_IN_SECONDS) {
+    logger.debug(`WatchCache#setUser: enter`);
+    logger.debug(`WatchCache#setUser: user: ${JSON.stringify(user, null, 2)}`);
     if (!user) {
       return;
     }
     const users = await this.getUsers(experimentId);
     const updatedUsers = users ? users : [];
+    logger.debug(`WatchCache#setUser: Current users: ${JSON.stringify(updatedUsers, null, 2)}`);
     updatedUsers.push(user);
 
     const key = this.getKey(experimentId);
+    logger.debug(`WatchCache#setUser: Store in ${key}: ${JSON.stringify(updatedUsers, null, 2)}`);
     Cache.setJson(key, updatedUsers, expiry);
   }
 
   async getUsers(experimentId) {
+    logger.debug(`WatchCache#getUsers: enter`);
     const key = this.getKey(experimentId);
+    logger.debug(`WatchCache#getUsers: key: ${key}`);
     return await Cache.getJson(key);
   }
 
   async delete(experimentId) {
+    logger.debug(`WatchCache#delete: enter`);
     const key = this.getKey(experimentId);
+    logger.debug(`WatchCache#delete: key: ${key}`);
     return await Cache.delete(key);
   }
 }
